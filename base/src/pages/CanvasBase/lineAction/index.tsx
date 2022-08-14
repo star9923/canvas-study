5f2dbeb9d02a99c48b120b626dd463fe18a101ac
star9923 <twx1243941653@163.com>
2022-08-14 15:57:51 +0800

    线条操作

--- base/src/pages/CanvasBase/lineAction/index.tsx
+++ base/src/pages/CanvasBase/lineAction/index.tsx
@@ -15,6 +15,7 @@ const Line = () => {
   const showLineWidth = () => {
     clear();
     const ctx = canvasRef?.current?.getContext('2d');
+    ctx?.setLineDash([1, 0]);
     // lineWidth 为 5 我们必须使用beginPath()方法来开始一个新的路径，才可以定义一个新的lineWidth属性。
     ctx?.beginPath();
     ctx!.lineWidth = 5;
@@ -40,7 +41,58 @@ const Line = () => {
     // 定义线条开始和结束的时候线帽效果
     clear();
     const ctx = canvasRef?.current?.getContext('2d');
+    ctx!.lineWidth = 16;
     ctx?.beginPath();
+    /**
+     * butt 默认值 无线帽
+     * round 圆形线帽
+     * square 正方形线帽
+     */
+    ctx!.lineCap = 'butt';
+    ctx?.moveTo(20, 20);
+    ctx?.lineTo(180, 20);
+    ctx?.stroke();
+    // lineWidth 为 10
+    ctx?.beginPath();
+    ctx!.lineCap = 'round';
+    ctx!.lineWidth = 10;
+    ctx?.moveTo(20, 40);
+    ctx?.lineTo(180, 40);
+    ctx?.stroke();
+    // lineWidth 为 15
+    ctx?.beginPath();
+    ctx!.lineCap = 'square';
+    ctx!.lineWidth = 15;
+    ctx?.moveTo(20, 60);
+    ctx?.lineTo(180, 60);
+    ctx?.stroke();
+    ctx?.closePath();
+  };
+
+  const showLineJoin = () => {
+    // 定义线条交接处的样式
+    clear();
+    const ctx = canvasRef?.current?.getContext('2d');
+    ctx?.beginPath();
+    ctx?.moveTo(50, 50);
+    ctx?.lineTo(100, 50);
+    ctx?.lineTo(50, 100);
+    ctx?.lineTo(100, 100);
+    // ctx!.lineJoin = 'miter'; // 默认值 尖角
+    ctx!.lineJoin = 'bevel'; // 折角
+    // ctx!.lineJoin = 'round'; // 圆角
+    ctx!.lineWidth = 12;
+    ctx?.stroke();
+    ctx?.closePath();
+  };
+
+  const showSetLineDash = () => {
+    // 设置线条虚实效果
+    clear();
+    const ctx = canvasRef?.current?.getContext('2d');
+    ctx?.setLineDash([5, 5]);
+    ctx!.lineWidth = 3;
+    ctx?.strokeRect(50, 50, 80, 80);
   };
 
   useEffect(() => {
@@ -55,6 +107,8 @@ const Line = () => {
       <div className={styles.actionContainer}>
         <span onClick={showLineWidth}>lineWidth属性效果</span>
         <span onClick={showLineCap}>lineCap属性效果</span>
+        <span onClick={showLineJoin}>lineJoin属性效果</span>
+        <span onClick={showSetLineDash}>setLineDash方法</span>
         <span onClick={clear}>清空画布</span>
       </div>
       <canvas ref={canvasRef} className={styles.canvas}>
